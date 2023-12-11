a0c7f4d26950ff6a0a3236363a4cc53de88c53a5
KrustyK99 <122903086+KrustyK99@users.noreply.github.com>
2023-12-11 15:09:05 -0500

    Added documentation to parameterreader.py

--- Python Code/parameterreader.py
+++ Python Code/parameterreader.py
@@ -4,24 +4,44 @@ import yaml
 class ParameterReader:
     """
     A class to read parameters from a YAML file.
+
+    Parameters:
+        filename (str): The name of the YAML file to read; typically "config.yaml"
     """
     def __init__(self, filename):
-        # Get the directory where this script is located
+        """
+        Initializes a new instance of the ParameterReader class.
+
+        Parameters:
+            filename (str): The name of the YAML file from which to read parameters.
+        """
         script_dir = os.path.dirname(os.path.abspath(__file__))
         self.filename = os.path.join(script_dir, filename) # the name of the YAML file
         self.parameters = {} # a dictionary to store the parameters and values
 
     def read_file(self):
-        # open the file in read mode
-        with open(self.filename, "r") as f:
-            # load the YAML file into the parameters dictionary
-            self.parameters = yaml.safe_load(f)
+        """
+        Reads parameters from the YAML file specified in the filename attribute.
+
+        Raises:
+            RuntimeError: If the file cannot be read.
+        """
+        try: 
+            # open the file in read mode
+            with open(self.filename, "r") as f:
+                # load the YAML file into the parameters dictionary
+                self.parameters = yaml.safe_load(f)
+        except Exception as e:
+            raise RuntimeError(f'Failed to read configuration file: {self.filename}. Error: {str(e)}')
 
     def get_parameter(self, key):
-        # return the value of the parameter if it exists, otherwise None
-        return self.parameters.get(key, None)
-    
+        """
+        Returns the value of the specified parameter.
 
-cls = ParameterReader("config.yaml")
-cls.read_file()
-print(f'database type: {cls.get_parameter("ui_default_date")}')
+        Parameters:
+            key (str): The name of the parameter.
+
+        Returns:
+            The value of the parameter, or None if the parameter is not found.
+        """
+        return self.parameters.get(key, None)
