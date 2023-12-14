d04ae92cede3ddf5ed454ece4fb520b8f868abb9
KrustyK99 <122903086+KrustyK99@users.noreply.github.com>
2023-12-14 10:36:22 -0500

    Removed old function (capture_filename_password) that wasn't being used anymore. This function was superceded by: capture_filename_password02.
    Added code to remove the hard return that was in being captured by regex.
    
    Refined function database_update to only update the filename and password fields if they are blank. If either has a value, nothing is done.

--- Python Code/fn_pw_cap.py
+++ Python Code/fn_pw_cap.py
@@ -5,7 +5,7 @@ from parameterreader import ParameterReader
 conn = None  # Declare conn as a global variable 
 
 class Filename_Password_Capture:
-    def __init__(self, db_type, nzb_id):
+    def __init__(self, db_type):
         """
         Initializes a new instance of the Filename_Password_Capture class.
 
@@ -18,41 +18,6 @@ class Filename_Password_Capture:
         """
         self.db_type = db_type
         self.conn = nzb_search_connection(db_type).create_connection()        
-        self.nzb_id = nzb_id
-
-    def capture_filename_password(self):
-        """
-        Captures the filename and password from the note field in the database.
-
-        Returns: 
-            A tuple containing the filename and password.
-
-        Raises:
-            RuntimeError: If the file cannot be read.
-        """
-        self.pr = ParameterReader("config.yaml")
-        self.pr.read_file()
-        self.re_patthern = self.pr.get_parameter("re_pattern")
-        self.str_sql = 'SELECT m.ID, m.note FROM movies m WHERE (m.ID=?);'
-        self.cur = self.conn.cursor()
-        self.cur.execute(self.str_sql, (self.nzb_id,))
-        self.row = self.cur.fetchall()
-        self.note = self.row[0][1]
-
-        # Initialize filename and password as not found
-        self.nzb_filename = "<No Filename Found>"
-        self.nzb_password = "<No Password Found>"
-
-        # Split the note into lines and process each line
-        for line in self.note.split('\n'):
-            self.match = re.search(self.re_patthern, line)
-            if self.match:
-                if self.match.group(1) == "File Name":
-                    self.nzb_filename = self.match.group(2)
-                elif self.match.group(1) == "File Pass":
-                    self.nzb_password = self.match.group(2)
-
-        return self.nzb_filename, self.nzb_password
     
     def capture_filename_password02(self, nzb_id):
         self.nzb_id = nzb_id
@@ -78,9 +43,9 @@ class Filename_Password_Capture:
             self.match = re.search(self.re_patthern, line)
             if self.match:
                 if self.match.group(1) == "File Name":
-                    self.nzb_filename = self.match.group(2)
+                    self.nzb_filename = self.match.group(2).replace('\r','')
                 elif self.match.group(1) == "File Pass":
-                    self.nzb_password = self.match.group(2)
+                    self.nzb_password = self.match.group(2).replace('\r','')
 
         return self.nzb_filename, self.nzb_password
 
@@ -97,23 +62,29 @@ class Filename_Password_Capture:
         """
         self.nzb_date = nzb_date
         self.nzb_series = nzb_series
-        self.str_sql = 'SELECT m.ID FROM movies m WHERE (m.download_date=?) AND (m.series_id=?);'
+        self.str_sql = 'SELECT m.ID, m.filename, m.password, m.note FROM movies m WHERE (m.download_date=?) AND (m.series_id=?);'
         self.cur = self.conn.cursor()
         self.cur.execute(self.str_sql, (self.nzb_date, self.nzb_series))
         self.row = self.cur.fetchall()
         print(f'Number of rows: {len(self.row)}')
         for rw in self.row:
-            #print(f'ID: {rw[0]}, {self.capture_filename_password02(rw[0])[0]}, {self.capture_filename_password02(rw[0])[1]}')
-            self.str_sql_update = 'UPDATE movies SET filename=?, password=? WHERE (ID=?);'
-            self.cur.execute(self.str_sql_update, (self.capture_filename_password02(rw[0])[0], self.capture_filename_password02(rw[0])[1], rw[0]))
-            self.conn.commit()
+            #Check to see if filename and password not empty
+            if rw[1] is None and rw[2] is None and rw[3] is not None:
+                self.str_sql_update = 'UPDATE movies SET filename=?, password=? WHERE (ID=?);'
+                fn = self.capture_filename_password02(rw[0])[0]
+                pw = self.capture_filename_password02(rw[0])[1]
+
+                self.cur.execute(self.str_sql_update, (fn, pw, rw[0]))
+                self.conn.commit()
         self.conn.close()
 
 try:
-    cls = Filename_Password_Capture(1, 5209)
+    cls = Filename_Password_Capture(1)
+    pr = ParameterReader("config.yaml")
+    pr.read_file()
     #print(f'cls.db_type: {cls.db_type}')
     #nzb_filename, nzb_password = cls.capture_filename_password()
     #print(f'Filename, Password: {nzb_filename}, {nzb_password}')
-    cls.database_upate('2023-12-13', 2)
+    cls.database_upate(pr.get_parameter("nzb_capture_date"), pr.get_parameter("nzb_capture_sid"))
 except Exception as e:
     print(f'Error: {e}')
