53a784e24d14746eea0a58fe2928c68432dd5e28
KrustyK99 <122903086+KrustyK99@users.noreply.github.com>
2023-12-22 16:44:19 -0500

    Added console script to setup.py.     - After application is installed, the user can run the application       from the command line by typing 'nzb_app' and pressing enter.

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name='nzb_application',
     version='0.1',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'nzb_app=python_code.api_nzb_search_main:main',
+        ],
+    },
 )
